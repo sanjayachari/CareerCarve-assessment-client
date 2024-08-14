2daf77ff3ef7ab13d0243d2329460a1c77618629
sanjay <devsanjay7676@gmail.com>
2024-08-14 13:21:56 +0530

    update for production

--- src/api/constant.js
+++ src/api/constant.js
@@ -1,3 +1,4 @@
-const SERVER = "http://localhost:5000/server/v1"
+// const SERVER = "http://localhost:5000/server/v1"
+const SERVER = "https://careercarve-assessment-server.onrender.com"
 
 export   {SERVER}
