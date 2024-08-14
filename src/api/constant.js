ef3342044c0b7b5f697989633012439c2922ceeb
sanjay <devsanjay7676@gmail.com>
2024-08-14 14:01:38 +0530

    cors

--- src/api/constant.js
+++ src/api/constant.js
@@ -1,4 +1,4 @@
 // const SERVER = "http://localhost:5000/server/v1"
-const SERVER = "https://careercarve-assessment-server.onrender.com"
+const SERVER = "https://careercarve-assessment-server.onrender.com/server/v1"
 
 export   {SERVER}
